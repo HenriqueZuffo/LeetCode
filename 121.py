6e8b9747ff448b3d47cc45cd16ee029cbb04307f
Henrique Gabriel de Andrade <henrique.andrade@wmcsistemas.com>
2024-05-31 19:16:41 -0300

    121 - Best Time to Buy and Sell Stock

---
+++ 121.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        if len(prices) == 0: return 0;
+
+        profit = 0
+        min_buy = prices[0]
+
+        for i in range(len(prices)):
+            profit = max(profit, prices[i] - min_buy)
+            min_buy = min(min_buy, prices[i])
+
+        return profit
+                
+            
+
+
+s = Solution()
+print(s.maxProfit(prices = [1,2]))
